0e90b6c81345360c440506feab69bb29cc463b9f
bammaster <bammasta@gmail.com>
2013-08-30 00:46:39 -0700

    Added methods for saving,deleting,and loading carts from storage.

--- PolyMeal/src/main/java/com/mustangexchange/polymeal/CartActivity.java
+++ PolyMeal/src/main/java/com/mustangexchange/polymeal/CartActivity.java
@@ -4,6 +4,7 @@ import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -31,7 +32,7 @@ public class CartActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_cart);
-
+        SharedPreferences appSharedPrefs = getSharedPreferences("PolyMeal",MODE_PRIVATE);
         lv = (ListView)findViewById(R.id.listView);
         lv.setAdapter(new CartItemAdapter(this, Cart.getCart(), Cart.getCartMoney()));
 
