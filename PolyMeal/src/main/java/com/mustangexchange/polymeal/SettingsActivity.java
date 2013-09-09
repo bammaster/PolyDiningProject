ee2119ad8048ed7af094bd871cb488f314d61ac3
Jon Amireh <jon.amireh@gmail.com>
2013-09-08 22:51:43 -0700

    "What's a meal worth?" integrated

--- PolyMeal/src/main/java/com/mustangexchange/polymeal/SettingsActivity.java
+++ PolyMeal/src/main/java/com/mustangexchange/polymeal/SettingsActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceScreen;
 import android.view.Menu;
 import android.view.View;
 import android.widget.TextView;
@@ -20,14 +21,38 @@ public class SettingsActivity extends PreferenceActivity {
     private String[] mealTimes = {"Breakfast","Lunch","Dinner","Late Night","Automatic"};
     private TextView current;
     public Preference timePref;
+    private Activity mActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.settings);
+        mActivity = this;
         timePref = findPreference("currentTime");
         timePref.setTitle(mealTimes[MoneyTime.calcRealTime()]);
-        timePref.setSelectable(false);
+        //timePref.setSelectable(false);
+        Preference myPref = (Preference) findPreference("currentTime");
+        myPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            public boolean onPreferenceClick(Preference preference) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(mActivity);
+                builder
+                        .setTitle("What's a meal worth?")
+                        .setMessage(
+                                "Breakfast: $7.90 (7:00-10:00am)\n"
+                                        + "Lunch: $9.00 (10:00am-5pm)\n"
+                                        + "Dinner: $10.75 (5-8:14pm)\n"
+                                        + "Late Night: $8.75 (8:15pm-2am)\n\n"
+                                        + "Meals reset weekly on Saturday morning at 2:00am")
+                        .setNeutralButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        });
+                builder.create().show();
+                return true;
+            }
+        });
         /*setContentView(R.layout.activity_settings);
         current = (TextView)findViewById(R.id.textCurrent);
         current.setText(mealTimes[MoneyTime.whichTime]);*/
