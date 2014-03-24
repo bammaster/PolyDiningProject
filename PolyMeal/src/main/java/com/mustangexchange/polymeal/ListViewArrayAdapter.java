b1c61464f7839dccb5da71715a7b8a218adadbde
Jon Amireh <jon.amireh@gmail.com>
2014-03-24 14:32:23 -0700

    Padding added to PlusDollarsActivity and bolding in Drawer enabled

--- PolyMeal/src/main/java/com/mustangexchange/polymeal/ListViewArrayAdapter.java
+++ PolyMeal/src/main/java/com/mustangexchange/polymeal/ListViewArrayAdapter.java
@@ -26,13 +26,42 @@ public class ListViewArrayAdapter extends ArrayAdapter<String> {
         font = Typeface.createFromAsset(context.getAssets(), "fonts/Roboto-Light.ttf");
     }
 
+    protected boolean callingClass(int position) {
+        String parentClass = context.getClass().getSimpleName();
+        switch (position)
+        {
+            case 1:
+                if(parentClass.equals("PolyMealActivity"))
+                    return true;
+                break;
+            case 2:
+                if(parentClass.equals("PlusDollarsActivity"))
+                    return true;
+                break;
+            case 3:
+                if(parentClass.equals("TransactionActivity"))
+                    return true;
+                break;
+            case 4:
+                if(parentClass.equals("TransactionActivity"))
+                    return true;
+                break;
+            default:
+                break;
+        }
+        return false;
+    }
+
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         convertView = inflater.inflate(R.layout.drawer_list_item, parent, false);
         TextView drawerItem = (TextView) convertView.findViewById(R.id.text1);
         drawerItem.setText(strings.get(position));
-        drawerItem.setTypeface(font);
+        if(callingClass(position))
+            drawerItem.setTypeface(font, Typeface.BOLD);
+        else
+            drawerItem.setTypeface(font);
         return convertView;
 
     }
