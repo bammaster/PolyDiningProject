a692d21b8e5e5409fc9b8ac20b40f54751e762a9
Blake <blake@themotlcode.com>
2014-09-22 17:27:35 -0700

    Added transaction comparators for date and time.

---
+++ polydining/src/main/java/com/themotlcode/polydining/Sorting/TransactionDateComparator.java
@@ -0,0 +1,19 @@
+package com.themotlcode.polydining.Sorting;
+
+import com.themotlcode.polydining.models.AccountTransaction;
+
+import org.joda.time.DateTime;
+
+import java.util.Comparator;
+
+/**
+ * Used to compare the dates of two transactions to determines their natual ordering.
+ */
+public class TransactionDateComparator implements Comparator<AccountTransaction> {
+    @Override
+    public int compare(AccountTransaction a, AccountTransaction b)
+    {
+        //Poor implementation. Needs to compare Date Times. Involves parsing String.
+        return a.getDate().compareTo(b.getDate());
+    }
+}
---
+++ polydining/src/main/java/com/themotlcode/polydining/Sorting/TransactionTypeComparator.java
@@ -0,0 +1,16 @@
+package com.themotlcode.polydining.Sorting;
+
+import com.themotlcode.polydining.models.AccountTransaction;
+
+import java.util.Comparator;
+
+/**
+ * Used to compare the types of two transactions to determines their natural ordering.
+ */
+public class TransactionTypeComparator implements Comparator<AccountTransaction> {
+    @Override
+    public int compare(AccountTransaction a, AccountTransaction b)
+    {
+        return a.getType().compareTo(b.getType());
+    }
+}
